7e45674ebf22b262065cc16c32a9b7701ef3c3a4
Alexander Ruf <alexander@alexruf.net>
2019-10-24 22:08:53 +0200

    Squashed a few commits
    - Initialize Go Modules
    - Implement basic REPL
    - Extend lexer to parse additional tokens
    - Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# monkey
+# Monkey
+
 An interpreter for the Monkey programming language written in [Go](https://golang.org/).
+
+Since this project is only for learning purpose, the results won't be production ready.
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"gitlab.com/alexruf/monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", usr.Username)
+	fmt.Print("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
