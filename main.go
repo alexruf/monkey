dbb6555b5446485194fbb7447b1f69737c8ee63a
Alexander Ruf <alexander@alexruf.net>
2021-06-11 23:39:12 +0200

    chore(ci): add GoReleaser configuration
    The Actions workflow can follow later.

--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 	"github.com/alexruf/monkey/repl"
 )
 
+// nolint
+var (
+	version = "dev"
+	commit  = ""
+	date    = ""
+	builtBy = ""
+)
+
 func main() {
 	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", usr.Username)
+	fmt.Printf("Hello %s! This is the Monkey programming language! (%s)\n", usr.Username, version)
 	fmt.Print("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
